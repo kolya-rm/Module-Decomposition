dd4bc4099bf86fce27262a3d1eaea5d989cd3e3f
Nikolai Romanov <kolya.rm@gmail.com>
2026-05-17 17:37:57 +0200

    Implemented send message to server.

--- chat-app/backend/chat-server.mjs
+++ chat-app/backend/chat-server.mjs
@@ -6,7 +6,7 @@ const APP = express();
 const PORT = 3000;
 
 
-const EXAMPLE = [
+const MESSAGES = [
   { user: "Tony", text: "Hi!" },
   { user: "Agnes", text: "Hello!" },
 ];
@@ -16,7 +16,45 @@ APP.use(cors());
 
 APP.get("/", (req, res) => {
   console.log("Received a request for messages");
-  res.send(JSON.stringify(EXAMPLE));
+  res.send(JSON.stringify(MESSAGES));
+});
+
+APP.post("/", (req, res) => {
+  const bodyBytes = [];
+
+  req.on("data", chunk => bodyBytes.push(...chunk));
+  req.on("end", () => {
+    const bodyString = String.fromCharCode(...bodyBytes);
+    let body;
+
+    try {
+      body = JSON.parse(bodyString);
+    } catch(error) {
+      console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
+      res.status(400).send("Expected body to be JSON");
+      return;
+    }
+    if (typeof body != "object" || !("user" in body) || !("text" in body)) {
+      console.error(`Failed to extract user and text from post body: ${body}`);
+      res.status(400).send("Expected body to be a JSON object contains keys quote and author");
+      return;
+    }
+    if (!body.user.length) {
+      console.error(`User is empty.`);
+      res.status(400).send("Expected user to be a non-empty string");
+      return;
+    }
+    if (!body.text.length) {
+      console.error(`Text is empty.`);
+      res.status(400).send("Expected text to be a non-empty string");
+      return;
+    }
+    MESSAGES.push({
+      user: body.user,
+      text: body.text,
+    });
+    res.send("ok");
+  });
 });
 
 APP.listen(PORT, () =>{
--- chat-app/frontend/script.mjs
+++ chat-app/frontend/script.mjs
@@ -1,32 +1,77 @@
 const SERVER_URL = "http://127.0.0.1:3000";
 
 function onWindowLoad() {
-  showMessages()
+  showMessages();
+  document.getElementById("btn-send").onclick = sendMessage;
 }
 
 function showMessages() {
   fetch(SERVER_URL)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        response.text().then(
-          text => alert(`Error occurred: ${text}`)
-        )
+        response.text().then((text) => {
+          console.error(`Error fetching messages, server response: ${text}`);
+          alert(`Error occurred: ${text}`);
+        });
       } else {
-        response.json().then(
-          data => redrawMessages(data)          
-        )
+        response.json().then((data) => redrawMessages(data));
       }
+    })
+    .catch((error) => {
+      console.error(`Error fetching messages from server: ${text}`);
+      alert(`Error occurred: ${text}`);
     });
 }
 
 function redrawMessages(data) {
   const chatElement = document.getElementById("chat");
   chatElement.innerHTML = "";
-  for(const message of data) {
+  for (const message of data) {
     const messageElement = document.createElement("p");
     messageElement.innerHTML = `${message.user}: ${message.text}`;
     chatElement.append(messageElement);
   }
+}
+
+function sendMessage() {
+  const user = document.getElementById("username").value;
+  const text = document.getElementById("message").value;
+
+  if (!user) {
+    alert("Error: User is empty!");
+    return;
+  }
+  if (!text) {
+    alert("Error: Text is empty!");
+    return;
+  }
+
+  fetch(SERVER_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      user: user,
+      text: text,
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        showMessages();
+        return;
+      } else {
+        response.text().then((text) => {
+          console.error(`Error on POST message, server response: ${error}`);
+          alert(`Error occurred: ${text}`);
+        });
+        return;
+      }
+    })
+    .catch((error) => {
+      console.error(`Error on POST message: ${error}`);
+      alert(`Something went wrong: ${error}`);
+    });
 }
 
 window.onload = onWindowLoad;
